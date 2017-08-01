ec530fcc9562af4998aeaf94fbdde321e1f272e5
Derek Howard <derek@howderek.com>
2017-08-01 11:59:19 -0500

    Initial Commit

---
+++ Dicebox/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Dicebox
+//
+//  Created by Derek Howard on 8/1/17.
+//  Copyright © 2017 Derek Howard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
